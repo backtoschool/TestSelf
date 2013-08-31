3b54ada1f3ed2f017a1c025a7d02387fddba6fae
backtoschool <serra715@live.com>
2013-08-31 17:54:11 +0700

    Miracle: First Page Splash Screen and goto main menu

--- TestSelf/src/main/java/com/backtoschool/testself/MainActivity.java
+++ TestSelf/src/main/java/com/backtoschool/testself/MainActivity.java
@@ -1,7 +1,9 @@
 package com.backtoschool.testself;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Handler;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -13,10 +15,25 @@ public class MainActivity extends Activity {
 
     // test merage kea test
 
+    private final int SPLASH_DISPLAY_LENGHT = 1000;
+
+    /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this, MainMenu.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
     }
 
 
---
+++ TestSelf/src/main/java/com/backtoschool/testself/MainMenu.java
@@ -0,0 +1,14 @@
+package com.backtoschool.testself;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MiracleLife on 31/8/2556.
+ */
+public class MainMenu extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
