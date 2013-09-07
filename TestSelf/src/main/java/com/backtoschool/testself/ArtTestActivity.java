ee9f822758fe5807fe424eecf3fb7441d815e660
backtoschool <serra715@live.com>
2013-09-07 23:18:33 +0700

    addtest

---
+++ TestSelf/src/main/java/com/backtoschool/testself/ArtTestActivity.java
@@ -0,0 +1,13 @@
+package com.backtoschool.testself;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MiracleLife on 7/9/2556.
+ */
+public class ArtTestActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- TestSelf/src/main/java/com/backtoschool/testself/TestMenuActivity.java
+++ TestSelf/src/main/java/com/backtoschool/testself/TestMenuActivity.java
@@ -1,14 +1,53 @@
 package com.backtoschool.testself;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by MiracleLife on 6/9/2556.
  */
-public class TestMenuActivity extends Activity {
+public class TestMenuActivity extends Activity implements View.OnClickListener {
+
+    private ImageView btntest1,btntest2;
+    private Intent objIntent;
+
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_layout);
+        initialWidget();
+        btntest1.setOnClickListener(this);
+        btntest2.setOnClickListener(this);
+
+    }
+
+    private void initialWidget() {
+        try{
+            btntest1 = (ImageView) findViewById(R.id.imgtest1);
+            btntest2 = (ImageView) findViewById(R.id.imgtest2);
+            Log.i("MainMenu", "initialWidget");
+        }
+        catch (Exception e){
+            Log.i("MainMenu", e.toString());
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgtest1:
+                objIntent = new Intent(TestMenuActivity.this,ScienceTestActivity.class);
+                startActivity(objIntent);
+                break;
+            case R.id.imgtest2:
+                objIntent = new Intent(TestMenuActivity.this,ArtTestActivity.class);
+                startActivity(objIntent);
+                break;
+
+        }
     }
 }
