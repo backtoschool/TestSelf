a0d50c869c46a91a5a4e99136180796e5c28d5d8
backtoschool <serra715@live.com>
2013-09-10 00:18:54 +0700

    dbClass

--- TestSelf/src/main/java/com/backtoschool/testself/myDBClass.java
+++ TestSelf/src/main/java/com/backtoschool/testself/myDBClass.java
@@ -1,6 +1,8 @@
 package com.backtoschool.testself;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -91,6 +93,85 @@ public class myDBClass extends SQLiteOpenHelper{
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    // Insert Data
+    public long InsertDataMember(String strName, String strLastname, String strEmail, String strSex,String strAge) {
+        // TODO Auto-generated method stub
+        try {
+            SQLiteDatabase db;
+            db = this.getWritableDatabase(); // Write Data
+            /**
+             *  for API 11 and above
+             SQLiteStatement insertCmd;
+             String strSQL = "INSERT INTO " + TABLE_MEMBER
+             + "(MemberID,Name,Tel) VALUES (?,?,?)";
+
+             insertCmd = db.compileStatement(strSQL);
+             insertCmd.bindString(1, strMemberID);
+             insertCmd.bindString(2, strName);
+             insertCmd.bindString(3, strTel);
+             return insertCmd.executeInsert();
+             */
+            ContentValues Val = new ContentValues();
+            Val.put("Name", strName);
+            Val.put("Lastname", strLastname);
+            Val.put("Email", strEmail);
+            Val.put("Sex", strSex);
+            Val.put("Age", strAge);
+
+            long rows = db.insert(TABLE_MEMBER, null, Val);
+
+            db.close();
+            return rows; // return rows inserted.
+
+        } catch (Exception e) {
+            return -1;
+        }
+
+    }
+    // Select Data Member
+    public String[] SelectDataMember(String strMemberID) {
+        // TODO Auto-generated method stub
+
+        try {
+            String arrData[] = null;
+
+            SQLiteDatabase db;
+            db = this.getReadableDatabase(); // Read Data
+
+            Cursor cursor = db.query(TABLE_MEMBER, new String[] { "*" },
+                    "MemberID=?",
+                    new String[] { String.valueOf(strMemberID) }, null, null, null, null);
+
+            if(cursor != null)
+            {
+                if (cursor.moveToFirst()) {
+                    arrData = new String[cursor.getColumnCount()];
+                    /***
+                     *  0 = MemberID
+                     *  1 = Name
+                     *  2 = Lastname
+                     *  3 = Email
+                     *  4 = Sex
+                     *  5 = Age
+                     */
+                    arrData[0] = cursor.getString(0);
+                    arrData[1] = cursor.getString(1);
+                    arrData[2] = cursor.getString(2);
+                    arrData[3] = cursor.getString(2);
+                    arrData[4] = cursor.getString(2);
+                    arrData[5] = cursor.getString(2);
+                }
+            }
+            cursor.close();
+            db.close();
+            return arrData;
+
+        } catch (Exception e) {
+            return null;
+        }
 
     }
 }
