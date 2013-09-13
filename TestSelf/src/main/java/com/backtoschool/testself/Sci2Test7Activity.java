366c8854b32dd626af4f6313064364e21b83e463
backtoschool <Miraclelife136>
2013-09-13 17:41:10 +0700

    add all sci test complete by top

--- TestSelf/src/main/java/com/backtoschool/testself/Sci2Test7Activity.java
+++ TestSelf/src/main/java/com/backtoschool/testself/Sci2Test7Activity.java
@@ -6,9 +6,9 @@ import android.os.Bundle;
 /**
  * Created by refill on 9/13/13.
  */
-public class Sci1Test1Activity extends Activity {
+public class Sci2Test7Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sci1test1_layout);
+        setContentView(R.layout.sci2test7_layout);
     }
 }
