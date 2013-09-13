b21db93ffcbd3c49048d9affdb1dc5da4a335ff6
backtoschool <Miraclelife136>
2013-09-13 15:41:48 +0700

    add sci test by top and edit art test layout by miracle

---
+++ TestSelf/src/main/java/com/backtoschool/testself/Sci1Test1Activity.java
@@ -0,0 +1,17 @@
+package com.backtoschool.testself;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by refill on 9/13/13.
+ */
+public class Sci1Test1Activity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sci1test1_layout);
+
+
+
+    }
+}
