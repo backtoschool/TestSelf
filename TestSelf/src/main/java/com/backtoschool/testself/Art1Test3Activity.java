3dd70e24c332fd9eda848a664fb9a614eae6e68f
COM <hatairat06@gmail.com>
2013-09-22 09:57:21 +0700

    add

--- TestSelf/src/main/java/com/backtoschool/testself/Art1Test3Activity.java
+++ TestSelf/src/main/java/com/backtoschool/testself/Art1Test3Activity.java
@@ -1,14 +1,103 @@
 package com.backtoschool.testself;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 /**
  * Created by MiracleLife on 7/9/2556.
  */
 public class Art1Test3Activity extends Activity {
+    private RadioButton radChoice1, radChoice2, radChoice3, radChoice4, radChoice5, radChoice6;
+    private String strAns = "";
+    private myDBClass objMyDBClass;
+
+    private Intent objIntent;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.art1test3_layout);
+
+        initialWidget();
+
+    }
+
+
+    public void initialWidget(){
+
+        radChoice1 = (RadioButton) findViewById(R.id.radioButton);
+        radChoice2 = (RadioButton) findViewById(R.id.radioButton2);
+        radChoice3 = (RadioButton) findViewById(R.id.radioButton3);
+        radChoice4 = (RadioButton) findViewById(R.id.radioButton4);
+
+    }
+
+
+    public void onClickNext(View view){
+
+
+        try{
+
+            if(radChoice1.isChecked()){
+
+                strAns = "0";
+
+            }else if(radChoice2.isChecked()){
+
+                strAns = "2";
+
+            }else if(radChoice3.isChecked()){
+
+                strAns = "3";
+
+            }else if(radChoice4.isChecked()){
+
+                strAns = "4";
+
+            }
+
+
+            if(strAns.equals("")){
+
+                Log.d("insertDB", "Have Check Button !!!!!");
+
+                Toast.makeText(Art1Test3Activity.this, "กรุณาเลือกคำตอบด้วยค่ะ",
+                        Toast.LENGTH_SHORT).show();
+
+
+            }else{
+
+                Log.d("insertDB", "insert DB Success !!!!!" + strAns);
+
+                UpdateDataSQLite();
+
+                objIntent = new Intent(Art1Test3Activity.this, Art1Test4Activity.class);
+                startActivity(objIntent);
+
+            }
+
+
+        }catch (Exception e){
+
+            Log.d("insertDB", "Art1Test3Activity Error " + e.toString());
+
+        }
+
+    }
+
+
+    public void UpdateDataSQLite(){
+
+        objMyDBClass = new myDBClass(this);
+        long insertID = objMyDBClass.Insert_Data_artscore_t1(null, strAns, null, null, null, null, null, null, null, null, null, null);
+
+
+
     }
 }
--- TestSelf/src/main/java/com/backtoschool/testself/Art2Test1Activity.java
+++ TestSelf/src/main/java/com/backtoschool/testself/Art2Test1Activity.java
@@ -1,14 +1,103 @@
 package com.backtoschool.testself;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 /**
  * Created by MiracleLife on 7/9/2556.
  */
 public class Art2Test1Activity extends Activity {
+    private RadioButton radChoice1, radChoice2, radChoice3, radChoice4, radChoice5, radChoice6;
+    private String strAns = "";
+    private myDBClass objMyDBClass;
+
+    private Intent objIntent;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.art2test1_layout);
+
+        initialWidget();
+
+    }
+
+
+    public void initialWidget(){
+
+        radChoice1 = (RadioButton) findViewById(R.id.radioButton);
+        radChoice2 = (RadioButton) findViewById(R.id.radioButton2);
+        radChoice3 = (RadioButton) findViewById(R.id.radioButton3);
+        radChoice4 = (RadioButton) findViewById(R.id.radioButton4);
+
+    }
+
+
+    public void onClickNext(View view){
+
+
+        try{
+
+            if(radChoice1.isChecked()){
+
+                strAns = "4";
+
+            }else if(radChoice2.isChecked()){
+
+                strAns = "3";
+
+            }else if(radChoice3.isChecked()){
+
+                strAns = "3";
+
+            }else if(radChoice4.isChecked()){
+
+                strAns = "0";
+
+            }
+
+
+            if(strAns.equals("")){
+
+                Log.d("insertDB", "Have Check Button !!!!!");
+
+                Toast.makeText(Art2Test1Activity.this, "กรุณาเลือกคำตอบด้วยค่ะ",
+                        Toast.LENGTH_SHORT).show();
+
+
+            }else{
+
+                Log.d("insertDB", "insert DB Success !!!!!" + strAns);
+
+                UpdateDataSQLite();
+
+                objIntent = new Intent(Art2Test1Activity.this, Art2Test2Activity.class);
+                startActivity(objIntent);
+
+            }
+
+
+        }catch (Exception e){
+
+            Log.d("insertDB", "Art2Test3Activity Error " + e.toString());
+
+        }
+
+    }
+
+
+    public void UpdateDataSQLite(){
+
+        objMyDBClass = new myDBClass(this);
+        long insertID = objMyDBClass.Insert_Data_artscore_t1(null, strAns, null, null, null, null, null, null, null, null, null, null);
+
+
+
     }
 }
