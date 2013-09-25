bd813f59337887342e46dd40be8a46a66d1d1169
backtoschool <serra715@live.com>
2013-09-25 13:58:10 +0700

    Add Login Layout and Activity

---
+++ TestSelf/src/main/java/com/backtoschool/testself/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.backtoschool.testself;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MiracleLife on 9/25/13.
+ */
+public class LoginActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
